e6ff13b4ad6a6f3406050c090d909081e03f7d4a
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-03-21 16:48:53 +0100

    fix: navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,34 +1,59 @@
-import { useState } from "react"
+import { useState } from "react";
+import bars from "../assets/bars.svg";
 
-const Navbar =() => {
+const Navbar = () => {
+  const [nav, setNav] = useState(true);
 
-    const [nav, setNav] = useState(true)
+  const HandleNav = () => {
+    setNav(!nav);
+  };
+  return (
+    <>
+      <div className="flex justify-between p-3 lg:justify-center">
+        <img
+          onClick={HandleNav}
+          className="h-10 w-10 lg:hidden cursor-pointer"
+          src={bars}
+          alt=""
+        />
+        <nav className="flex justify-center items-center lg:p-7">
+          <div>
+            <ul
+              className={
+                nav
+                  ? "list-none hidden h-[0.02rem] flex-col lg:flex lg:flex-row items-center gap-5 lg:h-3"
+                  : "absolute top-[4rem] left-0 w-screen shadow-2xl flex flex-col items-center justify-center bg-white"
+              }
+            >
+              <li className="p-3 font-semibold">
+                <a href="">Solutions</a>
+              </li>
+              <li className="p-3 font-semibold">
+                <a href="">Pricing</a>
+              </li>
+              <li className="p-3 font-semibold">
+                <a href="">Blog</a>
+              </li>
+              <li className="p-3 font-semibold">
+                <a href="">Support</a>
+              </li>
+              <li className="p-3 font-semibold">
+                <a href="">Resources</a>
+              </li>
+            </ul>
+          </div>
 
-    const HandleNav = () => {
-        setNav(!nav)
-    }
-    return(
-        <>
-        <div className="flex justify-between p-3 lg:justify-center">
-        <img onClick={HandleNav} className="h-10 w-10 lg:hidden cursor-pointer" src="src/assets/bars.svg" alt="" />
-            <nav className="flex justify-center items-center lg:p-7">
-                <div>
-                    <ul className={nav ? "list-none hidden h-[0.02rem] flex-col lg:flex lg:flex-row items-center gap-5 lg:h-3" : "absolute top-[4rem] left-0 w-screen shadow-2xl flex flex-col items-center justify-center bg-white"}>
-                        <li className="p-3 font-semibold"><a href="">Solutions</a></li>
-                        <li className="p-3 font-semibold"><a href="">Pricing</a></li>
-                        <li className="p-3 font-semibold"><a href="">Blog</a></li>
-                        <li className="p-3 font-semibold"><a href="">Support</a></li>
-                        <li className="p-3 font-semibold"><a href="">Resources</a></li>
-                    </ul>
-                </div>
-
-                <div className="lg:flex justify-end ml-44 gap-10">
-                    <button className="border-[#0088FF] hidden text-[#0088FF] border rounded-lg w-[4.5rem] h-10 p-[0.5rem] lg:flex">Login</button>
-                    <button className="border-[#0088FF] text-white border rounded-lg bg-[#0088FF] h-10 p-[0.5rem] lg:visible">Get Started</button>
-                </div>
-            </nav>
-        </div>
-        </>
-    )
-}
-export default Navbar
+          <div className="lg:flex justify-end ml-44 gap-10">
+            <button className="border-[#0088FF] justify-center hidden text-[#0088FF] border rounded-lg py-2 px-6 lg:flex">
+              Login
+            </button>
+            <button className="border-[#0088FF] py-2 px-6  justify-center text-white border rounded-lg bg-[#0088FF]  lg:visible">
+              Get Started
+            </button>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
+export default Navbar;
